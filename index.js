1ab5dbb8fe048beeffa113ab919ca396db330f0b
RayhanalKavey <rayhanalkavey@gmail.com>
2022-11-08 01:45:45 +0600

    add data for testing.

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5005;
+
+// middle ware
+app.use(cors());
+app.use(express.json());
+
+const services = require("./data/services.json");
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Photo Bizz");
+});
+app.get("/services", (req, res) => {
+  res.send(services);
+});
+app.listen(port, () => {
+  console.log(`Photo Bizz server is running on port: ${port}`);
+});

c587f844cd54d589464af62f454e2c9af2c90c02
RayhanalKavey <rayhanalkavey@gmail.com>
2022-11-08 20:51:07 +0600

    Connect to mongoDb with environment variable.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const app = express();
+require("dotenv").config();
 const port = process.env.PORT || 5005;
 
 // middle ware
@@ -9,6 +11,20 @@ app.use(express.json());
 
 const services = require("./data/services.json");
 
+//CONNECT to MongoDb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hufticd.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+//-------------------------
 app.get("/", (req, res) => {
   res.send("Welcome to Photo Bizz");
 });

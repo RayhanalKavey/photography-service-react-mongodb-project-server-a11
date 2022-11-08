58f4b5fdd0629bd3a8b08ea9a339fa8035e72c8f
RayhanalKavey <rayhanalkavey@gmail.com>
2022-11-08 23:17:07 +0600

    Get services data from mongodb.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
+require("colors");
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -9,8 +10,6 @@ const port = process.env.PORT || 5005;
 app.use(cors());
 app.use(express.json());
 
-const services = require("./data/services.json");
-
 //CONNECT to MongoDb
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hufticd.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -19,18 +18,45 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+
+async function dbConnect() {
+  try {
+    await client.connect();
+    console.log("Database connected".yellow.italic);
+  } catch (error) {
+    console.log(error.name.bgRed, error.message.bold);
+  }
+}
+dbConnect();
+
+//------------------------- workinG  End points
+const serviceCollection = client.db("photoBizz").collection("services");
+
+//get services data from the data base and send response to the client site
+app.get("/services", async (req, res) => {
+  try {
+    const query = {};
+    const cursor = serviceCollection.find(query);
+    const services = await cursor.toArray();
+    res.send({
+      success: true,
+      message: "Successfully got the data",
+      data: services,
+    });
+  } catch (error) {
+    console.log(error.name.bgRed, error.message.bold);
+    res.send({
+      success: false,
+      error: error.message,
+    });
+  }
 });
-//-------------------------
+
+//------------------------- End points end
 app.get("/", (req, res) => {
   res.send("Welcome to Photo Bizz");
 });
-app.get("/services", (req, res) => {
-  res.send(services);
-});
+
 app.listen(port, () => {
-  console.log(`Photo Bizz server is running on port: ${port}`);
+  console.log(`Photo Bizz server is running on port: ${port}`.cyan.italic);
 });
